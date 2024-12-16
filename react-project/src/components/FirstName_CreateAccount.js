5ac755f048212b0044b6e1eacec2490529a18e60
gharamzaki <s123403@stu.najah.edu>
2024-12-17 01:43:37 +0200

    creatyouraccount

---
+++ react-project/src/components/FirstName_CreateAccount.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+import '../styles/FirstName_CreateAccount.css';
+
+const FirstName_CreateAccount = ()=>{
+  return (
+    <input
+      type="text"
+      className="firstname-input"
+      name="first-name"
+      placeholder="*First name"
+    />
+  );
+}
+
+export default FirstName_CreateAccount;
