6c4e0bd1a0ece27979bf8ad0e62528c35b7901f3
HANA-IZZ <s12324223@stu.njah.edu>
2024-12-16 14:58:41 +0200

    main screen desc , about and courses card

---
+++ react-project/src/components/MainScreenDesc.js
@@ -0,0 +1,11 @@
+import '../styles/main-screen.css'
+const MainScreenDesc=()=>{
+    return(
+        <section className="main-screen-description">
+      <h1>مرحباً بك في موقعنا</h1>
+      <p>اكتسب مهارات جديدة وطور معرفتك من خلال الكورسات المتنوعة.</p>
+    </section>
+    );
+}
+
+export default MainScreenDesc;
