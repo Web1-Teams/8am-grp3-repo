29cfc67cf5790e1f74aa346fb58b001b6fd15133
Hadi-Haj-Ali <s12323671@stu.najah.edu>
2024-12-25 17:18:44 +0200

    reactDonation

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,9 +1,12 @@
-
-
-function App() {
+import DonationDate from "./components/DonationDate";
+import DonationAmount from "./components/DonationAmount";
+import DonationInput from "./components/DonationInput";
+export default function App() {
   return(
-    <div>My React App</div>
+    <div>
+      <DonationDate/>
+      <DonationAmount/>
+      <DonationInput/>
+    </div>
   );
 }
-
-export default App;
