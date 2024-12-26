5a0c82d8ad973566bb0672b24eb77ce3042eb958
mayarnazeeh <s12323342@stu.najah.edu>
2024-12-26 12:28:39 +0200

    mayar-

---
+++ react-project/src/components/FooterSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/forget.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <input type="submit" className="forget-password-2rd-pg-input-submit" value="Send"></input>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ react-project/src/components/HeaderSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/forget.css';
+
+const Header = () => {
+  return (
+    <header>
+      <label className="verification-title">Enter Verification Code</label>
+    </header>
+  );
+};
+
+export default Header;
