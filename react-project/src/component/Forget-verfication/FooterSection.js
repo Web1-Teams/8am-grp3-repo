574cd7fca893c46e4f216654095746d10611c8cc
mayarnazeeh <s12323342@stu.najah.edu>
2024-12-29 21:47:13 +0200

    Initial commit on new-react-mayar branch

---
+++ react-project/src/component/Forget-verfication/FooterSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './forget.css';
+
+function Footer  (){
+  return (
+    <footer className="footer">
+      <input type="submit" className="forget-password-2rd-pg-input-submit" value="Send"></input>
+    </footer>
+  )
+}
+
+export default Footer;
