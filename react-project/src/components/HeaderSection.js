3c1518c9b5fe36d3bc8697009fc9a43d6d0d047b
mayarnazeeh <s12323342@stu.najah.edu>
2024-12-30 20:08:05 +0200

    Add files via upload

---
+++ react-project/src/components/HeaderSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/forget.css';
+
+function Header(){
+  return (
+    <header>
+      <label className="verification-title">Enter Verification Code</label>
+    </header>
+  )
+}
+
+export default Header;
---
+++ react-project/src/components/VerificationSection.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import '../styles/forget.css';
+function Section (){
+  const [code, setCode] = useState(['', '', '', '']);
+
+  const updateCode = (position, event) => {
+    let updatedCode = code.map((item, idx) => 
+      idx === position ? event.target.value.slice(0, 1) : item
+    )
+    
+    setCode(updatedCode);
+
+    if (event.target.nextElementSibling && event.target.value) {
+      event.target.nextElementSibling.focus();
+    }
+  }
+
+  return (
+    <section className="verification-section">
+      {code.map((digit, idx) => (
+        <input
+          key={idx}
+          type="text"
+          maxLength="1"
+          className="circle-input"
+          value={digit}
+          onChange={(e) => updateCode(idx, e)}
+        />
+      ))}
+    </section>
+  )
+}
+
+export default Section;
