43b64cedc3c526afca2dea9ef0b7b486b99f6af1
mohidreekh <mohammad.idreekh00@gmail.com>
2025-01-02 00:15:06 +0200

    Login header and description and Create Account Link in React

---
+++ react-project/src/components/LoginHeader/LoginHeader.js
@@ -0,0 +1,11 @@
+import "./LoginHeader.css";
+
+const LoginHeader = () => {
+  return (
+    <header className="login-screen-header">
+      <h1>Edujoy</h1>
+    </header>
+  );
+};
+
+export default LoginHeader;
