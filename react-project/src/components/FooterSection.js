bde6e9b1fa935905ec2abaeb2734c470a5c095d4
mayarnazeeh <s12323342@stu.najah.edu>
2025-01-02 15:43:31 +0200

    Update FooterSection.js

--- react-project/src/components/FooterSection.js
+++ react-project/src/components/FooterSection.js
@@ -4,7 +4,9 @@ import './forget.css';
 function Footer  (){
   return (
     <footer className="footer">
+       <link to="">
       <input type="submit" className="forget-password-2rd-pg-input-submit" value="Send"></input>
+        </link>
     </footer>
   )
 }
